1e1dd5b24a23ee276476c38cab975695c18cfeab
ByMikiii <mikixr1@gmail.com>
2022-03-21 22:13:06 +0100

    GameStore Begin

---
+++ GameStore/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./CSS/index-style.css">
+  <link rel="icon" type="icon" href="./Images/iconka.png">
+  <title>Game Store ByMikiii</title>
+  
+</head>
+<body>
+  <ul>
+    <li><a href="./pages/store.php">Store</a></li>
+    <li><a href="./pages/library.php">Library</a></li>
+    <li style="float:right"> <a href="">Profile</a></li>
+  </ul>
+  <p>MAIN PAGE</p>
+</body>
+</html>
