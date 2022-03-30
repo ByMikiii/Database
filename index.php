afac16d1ee385d160aa6b3daac716bc10d012389
ByMikiii <mikixr1@gmail.com>
2022-03-31 00:17:26 +0200

    Images resize + images not workin

--- index.php
+++ index.php
@@ -6,27 +6,41 @@ include('./parts/header.php')
 <main class="container">
 <p class=login-info> DU Carousel </p>
 
-<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
   <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="./Images/dudo.jpeg" class="d-block w-100" alt="DUDO">
+    <div class="item active">
+      <img src="./images/klubina.jpeg" alt="Los Angeles">
     </div>
-    <div class="carousel-item">
-      <img src="./Images/peepohappy.PNG" class="d-block w-100" alt="PEEPOHAPPY">
+
+    <div class="item">
+      <img src="./images/sleepin.jpeg" alt="Chicago">
     </div>
-    <div class="carousel-item">
-      <img src="./Images/sleepin.jpeg" class="d-block w-100" alt="...">
+
+    <div class="item">
+      <img src="./images/peepohappy.jpg" alt="New York">
     </div>
   </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
+
+  <!-- Left and right controls -->
+  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right"></span>
+    <span class="sr-only">Next</span>
+  </a>
 </div>
+
 </main>
 
   <?php include('./parts/footer.php')?>
