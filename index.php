d84b6042c07171504303642065105d19d09c9312
ByMikiii <75531503+ByMikiii@users.noreply.github.com>
2022-04-05 19:51:02 +0200

    Carousel workin

--- index.php
+++ index.php
@@ -4,41 +4,27 @@ include('./parts/header.php')
 ?>
 
 <main class="container">
-<p class=login-info> DU Carousel </p>
 
-
-<div id="myCarousel" class="carousel slide" data-ride="carousel">
-  <!-- Indicators -->
-  <ol class="carousel-indicators">
-    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-    <li data-target="#myCarousel" data-slide-to="1"></li>
-    <li data-target="#myCarousel" data-slide-to="2"></li>
-  </ol>
-
-  <!-- Wrapper for slides -->
+<div style="width: 500px;   margin-left: auto; margin-right: auto; margin-top: 80px;" id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
   <div class="carousel-inner">
-    <div class="item active">
-      <img src="./images/klubina.jpeg" alt="Los Angeles">
+    <div class="carousel-item active">
+      <img src="./images/peepohappy.jpg" class="d-block w-100" alt="Ležiaci pes">
     </div>
-
-    <div class="item">
-      <img src="./images/sleepin.jpeg" alt="Chicago">
+    <div class="carousel-item">
+      <img src="./images/sleepin.jpeg" class="d-block w-100" alt="Spiaci pes">
     </div>
-
-    <div class="item">
-      <img src="./images/peepohappy.jpg" alt="New York">
+    <div class="carousel-item">
+      <img src="./images/klubina.jpeg" class="d-block w-100" alt="Klubina">
     </div>
   </div>
-
-  <!-- Left and right controls -->
-  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-    <span class="glyphicon glyphicon-chevron-left"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="right carousel-control" href="#myCarousel" data-slide="next">
-    <span class="glyphicon glyphicon-chevron-right"></span>
-    <span class="sr-only">Next</span>
-  </a>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
 </div>
 
 </main>
