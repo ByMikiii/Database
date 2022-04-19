fdf60690ccde2e591dd98b873416edf6a6342ef0
ByMikiii <mikixr1@gmail.com>
2022-04-19 21:50:23 +0200

    List of Users Adjust

--- pages/zoznamPouzivatelov.php
+++ pages/zoznamPouzivatelov.php
@@ -19,9 +19,9 @@ require '../phpscripts/getAllUsers.php';
 
 <?php foreach ($users as $user) :  ?>
   <li> 
-    <span><?php echo $user["id"] ?></span>
-    <span> <a href="./profile.php?id=<?php echo $user["id"]?>" ><img class='profile-picture' src="/Database/Images/profile.png" alt="Profile Picture"></a></span>
-    <span><?php echo $user["username"] ?></span>
+    <span> <?php echo $user["id"] ?> </span>
+    <span> <img class='profile-picture' src="/Database/Images/profile.png" alt="Profile Picture"></></span>
+    <span> <a  href="./profile.php?id=<?php echo $user["id"]?>"><?php echo $user["username"] ?></a></span>
     <span><?php echo $user["email"] ?></span>
     <span><?php echo $user["created_at"] ?></span>
     <span><?php echo "Akcie" ?></span>
